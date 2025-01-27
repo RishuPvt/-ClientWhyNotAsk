291d495f89e4d9ca9c2d499131c9681470da73d7
Rishu Raj <rishuraj1127@gmail.com>
2025-01-27 19:55:57 +0530

    all components

---
+++ src/Componets/Categoery/AllComments.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../Header/Header2'
+const AllComments: React.FC=()=> {
+
+
+  return (
+    <>
+    <Header />
+    <div>AllComments</div>
+    
+    
+    </>
+  )
+}
+
+export default AllComments
